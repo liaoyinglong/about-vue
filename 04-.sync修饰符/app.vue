3c0064f0f0a0797bca8556f091f79143563e5cab
liaoyinglong <vigossliao@gmail.com>
2017-10-20 00:24:25 +0800

    add:.sync modifier

---
+++ 04-.sync修饰符/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <child :appValue.sync='appValue'></child>
+    <!-- <child :appValue='appValue' @update:appValue='val=> appValue = val'></child> -->
+    <br> child组件输入的value：{{appValue}}
+  </div>
+</template>
+
+<script>
+import child from './child'
+export default {
+  components: {
+    child,
+  },
+  data() {
+    return {
+      appValue: '',
+    }
+  },
+}
+</script>
---
+++ 04-.sync修饰符/child.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="text" @input="handleInput($event)">
+  </div>
+</template>
+<script>
+export default {
+  props: ['appValue'],
+  methods: {
+    handleInput(e) {
+      this.$emit('update:appValue', e.target.value)
+    },
+  },
+}
+</script>
