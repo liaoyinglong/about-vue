d9f8d21bc5abf5421c03efd4aef26285ce401100
liaoyinglong <vigossliao@gmail.com>
2017-10-22 00:03:05 +0800

    add:Single-Slot

---
+++ 05-slot/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    child组件
+    <child :list='list'>
+      <template slot-scope="scope">
+        <span>{{scope.row.name}}---{{scope.row.age}}</span>
+      </template>
+    </child>
+    <hr> 标签内没有写任何东西的 child组件
+    <child :list='list'></child>
+  </div>
+</template>
+
+<script>
+import child from './child'
+export default {
+  components: {
+    child,
+  },
+  data() {
+    return {
+      list: [{ name: 'a', age: 18 }, { name: 'b', age: 19 }, { name: 'c', age: 20 }, { name: 'd', age: 21 }],
+    }
+  },
+}
+</script>
---
+++ 05-slot/child.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for="(item,index) in list" :key="index">
+      <slot :row='item'>
+        如果没有写任何东西就显示这个 -- {{index}}
+      </slot>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: ['list'],
+}
+</script>
+
